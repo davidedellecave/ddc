3d4406a437a14de1e34c36fb9e56abc4216976f4
davidedellecave <davide.dellecave@gmail.com>
2015-10-05 10:41:42 +0200

    first

---
+++ java/jtest/src/ddc/servlet/t1.java
@@ -0,0 +1,38 @@
+package ddc.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class t1
+ * http://localhost:8080/jgottardo/t1
+ */
+public class t1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public t1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Hi Guys!!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Hi Guys!!");
+	}
+
+}
